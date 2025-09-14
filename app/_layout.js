6d008a5bd67b25b82a234af5873cadbb8cf8d9bf
Gabriel Zolk <dialixkat@gmail.com>
2025-09-13 23:16:31 -0300

    init

--- app/_layout.js
+++ app/_layout.js
@@ -1,8 +1,15 @@
 import { Stack } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider } from '../utils/ThemeContext';
+import { useEffect } from 'react';
+import { registerSW } from '../utils/registerSW';
 
 export default function RootLayout() {
+  useEffect(() => {
+    // Registrar Service Worker para PWA offline
+    registerSW();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <ThemeProvider>
