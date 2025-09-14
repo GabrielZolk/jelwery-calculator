a68d0c86107b63fd0522f8923b7df58482eb7ab7
Gabriel Zolk <dialixkat@gmail.com>
2025-09-13 23:10:38 -0300

    Fix build error: simplify metro config and manifest

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,4 @@
 const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 
-// Adicionar suporte a assets da pasta public
-config.resolver.platforms = ['ios', 'android', 'native', 'web'];
-
-// Garantir que arquivos da pasta public sejam incluídos
-config.resolver.assetExts.push('png', 'jpg', 'jpeg', 'gif', 'svg', 'json', 'js');
-
 module.exports = config;
